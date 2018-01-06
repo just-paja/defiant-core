613ea28bb4b2cb91ba68ae5172a87deb2b08b54f
Pavel Žák <pavel@berrycloud.cz>
2018-01-06 16:50:46 +0100

    Improve Field error message

--- lib/Model/Field.php
+++ lib/Model/Field.php
@@ -16,7 +16,7 @@ abstract class Field extends \Defiant\Resource\ClassCollector {
   public static function createFromDef($name, $def) {
     $fieldType = static::getFieldTypeByDef($def);
     if (!$fieldType) {
-      throw new Error('Could not find model by definition', $def);
+      throw new Error(sprintf('Could not find field "%s" by definition', $name), $def);
     }
     return new $fieldType($name, $def);
   }

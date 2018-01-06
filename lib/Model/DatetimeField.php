8f2b38eb0f78188e180fd6ef0b30fcba9b8e8b94
just-paja <just.paja@gmail.com>
2018-01-06 21:58:56 +0100

    Set default datetime values given no value is set

--- lib/Model/DatetimeField.php
+++ lib/Model/DatetimeField.php
@@ -8,11 +8,13 @@ class DatetimeField extends Field {
   protected $setNowOnUpdate = false;
 
   public function serialize($value, $opportunity = null) {
-    if ($this->setNowOnInsert && $opportunity === MODEL_INSERT) {
-      return (new \DateTime())->format('c');
-    }
-    if (!$value && $this->setNowOnUpdate && ($opportunity === MODEL_UPDATE || $opportunity === MODEL_INSERT)) {
-      return (new \DateTime())->format('c');
+    if (!$value) {
+      if ($this->setNowOnInsert && $opportunity === MODEL_INSERT) {
+        return (new \DateTime())->format('c');
+      }
+      if ($this->setNowOnUpdate && ($opportunity === MODEL_UPDATE || $opportunity === MODEL_INSERT)) {
+        return (new \DateTime())->format('c');
+      }
     }
     return $value;
   }

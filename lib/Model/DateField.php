098b717327cf600db7732e9a08154b74f4b0578a
just-paja <just.paja@gmail.com>
2018-01-12 01:45:33 +0100

    Validate Date input value

--- lib/Model/DateField.php
+++ lib/Model/DateField.php
@@ -11,4 +11,13 @@ class DateField extends Field {
     }
     return $value;
   }
+
+  public function validateValue($value) {
+    if ($value) {
+      if (!preg_match('/^[0-9]{4}\-[01]\d\-[0123]\d$/', $value)) {
+        throw new \Defiant\View\ValidationError('Value must be a valid ISO-8601 date.', $this->name);
+      }
+    }
+    return true;
+  }
 }

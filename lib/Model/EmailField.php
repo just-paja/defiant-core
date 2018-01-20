e8bbcdd1bcb5d29f1f65883b0d995d5fdb02dbd3
just-paja <just.paja@gmail.com>
2018-01-20 14:07:38 +0100

    Validate e-mail field values

--- lib/Model/EmailField.php
+++ lib/Model/EmailField.php
@@ -3,4 +3,15 @@
 namespace Defiant\Model;
 
 class EmailField extends VarcharField {
+  public function validateValue($value) {
+    if ($value) {
+      if (!filter_var($value, \FILTER_VALIDATE_EMAIL)) {
+        throw new \Defiant\View\ValidationError(sprintf(
+          'Provided value "%s" is not a valid e-mail address.',
+          $value
+        ), $this->name);
+      }
+    }
+    return true;
+  }
 }
